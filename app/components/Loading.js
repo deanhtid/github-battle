824df89363f9148e02e67ff4f2cdae85c8c9b497
deanhtid <deanhtid99@gmail.com>
2018-12-07 08:20:20 +0000

    Refactored to use Class Properties

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,13 +9,18 @@ const styles = {
 };
 
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  };
 
-    this.state = {
-      text: props.text
-    };
-  }
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  };
+  state = {
+    text: this.props.text
+  };
   componentDidMount() {
     const { text, speed } = this.props;
     const stopper = text + '...';
@@ -35,15 +40,5 @@ class Loading extends React.Component {
     return <p style={styles.content}>{this.state.text}</p>;
   }
 }
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-};
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
-};
 
 export default Loading;
